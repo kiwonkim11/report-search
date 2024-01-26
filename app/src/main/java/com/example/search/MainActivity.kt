7f1a68caadbcba0a1eccd7f4bc55e053f29236c1
kiwon kim <rlarldnjs17@gmail.com>
2024-01-26 15:50:50 +0900

    개인과제 A Type

---
+++ app/src/main/java/com/example/search/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.search
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.search.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val tabTextList = listOf(R.string.main_list_first, R.string.main_list_second)
+    private val tabIconList = listOf(R.drawable.ic_search2, R.drawable.ic_my)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        initViewPager()
+
+    }
+
+    private fun initViewPager() {
+        binding.viewpager2.adapter = ViewPagerAdapter(this)
+        TabLayoutMediator(binding.tab, binding.viewpager2) { tab, pos ->
+            tab.text = getString(tabTextList[pos])
+            tab.icon = getDrawable(tabIconList[pos])
+        }.attach()
+    }
+
+}
